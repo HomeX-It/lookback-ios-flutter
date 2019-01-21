22eae409d09be9ef9b2f4eb149790ebd62d761a0
Jason Rai <jrai@homex.com>
2019-01-21 15:58:26 -0500

    created plugin projects

---
+++ lookback_ios_flutter/ios/Classes/LookbackIosFlutterPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface LookbackIosFlutterPlugin : NSObject<FlutterPlugin>
+@end
---
+++ lookback_ios_flutter/ios/Classes/LookbackIosFlutterPlugin.m
@@ -0,0 +1,20 @@
+#import "LookbackIosFlutterPlugin.h"
+
+@implementation LookbackIosFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"lookback_ios_flutter"
+            binaryMessenger:[registrar messenger]];
+  LookbackIosFlutterPlugin* instance = [[LookbackIosFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
