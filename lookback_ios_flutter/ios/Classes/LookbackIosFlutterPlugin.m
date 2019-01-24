7ca618eaf0d16df36156518b89269c73f75bfaac
Jason Rai <jrai@homex.com>
2019-01-24 10:02:05 -0500

    lookback init working, need to handle links next

--- lookback_ios_flutter/ios/Classes/LookbackIosFlutterPlugin.m
+++ lookback_ios_flutter/ios/Classes/LookbackIosFlutterPlugin.m
@@ -1,20 +1,44 @@
 #import "LookbackIosFlutterPlugin.h"
 
+#import "Lookback.h"
+
 @implementation LookbackIosFlutterPlugin
+LookbackParticipate *_participate;
+- (LookbackParticipate*)participate
+{
+    if(!_participate) {
+        _participate = [LookbackParticipate new];
+    }
+    return _participate;
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"lookback_ios_flutter"
-            binaryMessenger:[registrar messenger]];
-  LookbackIosFlutterPlugin* instance = [[LookbackIosFlutterPlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"lookback_ios_flutter"
+                                     binaryMessenger:[registrar messenger]];
+    LookbackIosFlutterPlugin* instance = [[LookbackIosFlutterPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+    [registrar addApplicationDelegate:instance];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
+      [self.participate setupWithApplicationWindow: [[[UIApplication sharedApplication] windows] firstObject]];
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)options
+{
+    [self.participate setupWithApplicationWindow: [[[UIApplication sharedApplication] windows] firstObject]];
+    return YES;
+}
+
+- (BOOL)application:(UIApplication*)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+    return [self.participate startParticipationFromURL:url];
 }
 
 @end
