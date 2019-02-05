5b74d6513c88a60f9508c7a2fa50a4bcb864f871
Jason Rai <jrai@homex.com>
2019-02-05 11:21:08 -0500

    remove unused code

--- android/src/main/java/com/example/lookbackiosflutter/LookbackIosFlutterPlugin.java
+++ android/src/main/java/com/example/lookbackiosflutter/LookbackIosFlutterPlugin.java
@@ -16,10 +16,6 @@ public class LookbackIosFlutterPlugin implements MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
-    }
+    result.notImplemented();
   }
 }
